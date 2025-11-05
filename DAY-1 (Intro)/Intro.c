b0e9e01782cac900467a9f280c23026fffe2894d
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2025-11-05 20:52:31 +0530

    Day-1 (Introduction Of C)

---
+++ DAY-1 (Intro)/Intro.c
@@ -0,0 +1,12 @@
+// int = return type (inte value) , void = return type(null)
+
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Hello Students How are You!\n");
+    printf("How You feel!");
+
+    return 0;
+}
