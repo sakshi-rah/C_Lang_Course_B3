aafc367318842855a8bde387338b29e2c8ce0dd4
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2025-11-10 21:09:46 +0530

    Day-3(Arithmetic Operators Program)

---
+++ DAY-3 (Operators)/arithmetic_Opt.c
@@ -0,0 +1,33 @@
+//  1. Arithmetic operators (+,-,*,/,%)
+// ** 2. Increment Decrement Operators(++,--)
+//  3. Assignment operators (=,+=,-=,*=,/=,%=)
+//  4. Relational Operators (<,<=,>,>=,==,!=)
+//  5. Logical operators (&&,||,!)
+
+// Aritmetic Operators Program:-
+#include <stdio.h>
+int main()
+{
+    int a = 20, b = 30;
+    printf("a+b: %d\n", a + b); // a+b: 50
+    int sum = a + b;
+    printf("Sum of a & b is %d\n\n", sum);
+
+    printf("a-b: %d\n", a - b); // a-b: -10
+    int sub = a - b;            // 20-30 = -10
+    printf("Sub a & b is %d\n\n", sub);
+
+    printf("a*b: %d\n", a * b); // a*b: 600
+    int multi = a * b;          // 20*30 = 600
+    printf("Multi a & b is %d\n\n", multi);
+
+    printf("a%%b: %d\n", a % b); // a%b: 20
+    int mod = (a % b);             // 20%30 = 20
+    printf("Mod a & b is %d\n\n", mod);
+
+    printf("a/b: %f\n", (float)a/b);
+    float div = (float)a/b; // type casting (int- float) 
+    printf("Div a & b is %.2f\n", div);
+
+    return 0;
+}
