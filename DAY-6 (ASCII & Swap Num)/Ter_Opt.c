5e02c7452a2db9d080ef1871d79a64299ef76a83
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2025-11-15 08:54:26 +0530

    Day-6 (ASCII value and Ternary Operator)

---
+++ DAY-6 (ASCII & Swap Num)/Ter_Opt.c
@@ -0,0 +1,26 @@
+// Conditional/Ternary Operator(if-else)
+
+#include<stdio.h>
+
+int main(){
+    // Syntax: condition ? expression1 : expression2;
+    // expre1 = T, expre2 = F
+
+    // Finding the Number is Ever or Odd
+    int marks = 80;
+    (marks % 2 == 0) ? printf("Even\n") : printf("Odd\n");
+
+    // Finding Greater Number
+    int s = 350, r = 68;
+    int result = (s>r)? s : r; // (35>68) = F => Expression2 Run = r => r = 68
+    printf("Greater Number is %d\n", result); // 350
+
+    // Finding Smaller Number
+
+    int a = 100, b = 30;
+    int output = (a<b)? a : b; // (10<30) = T => Expression1 Run = a => a = 10
+    printf("Smaller Number is %d\n", output); // 10
+
+
+    return 0;
+}
---
+++ DAY-6 (ASCII & Swap Num)/ascii.c
@@ -0,0 +1,24 @@
+// ASCII Value (A to Z OR a to z) keyboards = 127 keys
+// A = 65; a = 97; // difference = 32
+// a = 97 - 32 = 65 = A
+// b = 98 - 32 = 66 = B 
+// 32 = space, 33 = !
+
+#include<stdio.h>
+
+int main(){
+char name = 'A';
+printf("ACSII Value of A is %d\n", name); // 65
+
+char character = 'z';
+printf("ACSII Value of z is %d\n", character); // 122
+
+char name1 = 'S';
+printf("ACSII Value of S is %d\n", name1); // 115-32 = 83
+
+int num = 75;
+printf("ACSII Value of num is %c", num);
+
+    return 0;
+}
+
